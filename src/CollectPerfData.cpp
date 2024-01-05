264921aedaabb85792fbf0d357b3906ad441799b
Felix <you@example.com>
2024-01-05 07:54:32 +0100

    script perf output

--- src/CollectPerfData.cpp
+++ src/CollectPerfData.cpp
@@ -1,4 +1,6 @@
 #include "CollectPerfData.h"
+#include <array>
+#include <memory>
 
 
 CollectPerfData::CollectPerfData(const std::string &options):options(options), collecting(false){}
@@ -22,10 +24,32 @@ void CollectPerfData::recordPerf()
 
 std::string CollectPerfData::retriveData()
 {
-    return std::string();
+    if (!collecting) {
+        throw "data is currently not collected";
+    }
+
+    system("perf record --stop");
+
+    collecting = false;
+
+    std::string perfData = scriptPerf("perf script");
+    return perfData;
 }
 
 std::string CollectPerfData::scriptPerf(const std::string &command)
 {
-    return std::string();
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(command.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw "popen() failed!";
+    }
+
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+    }
+
+    return result;
 }
