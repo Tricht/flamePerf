d466c1f13085c318542e5a4ef40226ee9034cef6
Felix Wolter <felix.wolter@stud.h-da.de>
2024-02-11 17:39:22 +0100

    initalize perfPID

--- src/CollectPerfData.cpp
+++ src/CollectPerfData.cpp
@@ -33,6 +33,8 @@ void CollectPerfData::handleSignal(int signum)
     exit(signum);
 }
 
+pid_t CollectPerfData::perfPID = 0;
+
 void CollectPerfData::recordPerf()
 {
     signal(SIGINT, CollectPerfData::handleSignal);
