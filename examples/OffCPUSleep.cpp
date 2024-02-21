e90de12738eacdeedd1291315b0b08c7e3dd5db4
Felix <you@example.com>
2024-02-21 17:49:44 +0100

    add OffCPU example

---
+++ examples/OffCPUSleep.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+
+void intensiveComputation() {
+    for (long i = 0; i < 10000; ++i) {
+        std::vector<int> temp(100, i);
+    }
+}
+
+void sleepFunction() {
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+}
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    intensiveComputation();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Computation duration " 
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms." << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    sleepFunction();
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "Sleep Function duration " 
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms." << std::endl;
+
+    return 0;
+}
