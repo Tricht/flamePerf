6d49c33710ef68b81dd9b50be29a08780fbffcb9
Felix Wolter <felix.wolter@stud.h-da.de>
2024-02-24 12:47:41 +0100

    adjust example

--- examples/OffCPUSleep.cpp
+++ examples/OffCPUSleep.cpp
@@ -3,28 +3,37 @@
 #include <chrono>
 #include <thread>
 
-
-void intensiveComputation() {
-    for (long i = 0; i < 10000; ++i) {
+void intensiveComputation()
+{
+    for (long i = 0; i < 10000; ++i)
+    {
         std::vector<int> temp(100, i);
     }
 }
 
-void sleepFunction() {
+void sleepFunction()
+{
     std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
-int main() {
+int main()
+{
     auto start = std::chrono::high_resolution_clock::now();
     intensiveComputation();
     auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Computation duration " 
+    std::cout << "Computation duration "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms." << std::endl;
 
     start = std::chrono::high_resolution_clock::now();
     sleepFunction();
     end = std::chrono::high_resolution_clock::now();
-    std::cout << "Sleep Function duration " 
+    std::cout << "Sleep Function duration "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms." << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    intensiveComputation();
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "Computation duration "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms." << std::endl;
 
     return 0;
